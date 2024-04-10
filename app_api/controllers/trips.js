c03e4f683ecbf7ce99edb84ffebdc5989fc9a174
Will <willmclaughlin4th@gmail.com>
2024-04-10 13:29:13 -0400

    Module 6 completed Baseline

--- app_api/controllers/trips.js
+++ app_api/controllers/trips.js
@@ -1,46 +1,92 @@
 const mongoose = require('mongoose');
-const Trip = require('../models/travlr');
+const Trip = require('../models/travlr'); // Check if this is the correct import path
 const Model = mongoose.model('trips');
 
-const tripsList = async(req, res) => {
-    const q = await Model
-        .find({})
-        .exec();
-
-
-    if(!q)
-    {
-        return res
-                .status(404)
-                .json(err);
-    }else{
-        return res 
-            .status(200)
-            .json(q);
+const tripsList = async (req, res) => {
+    try {
+        const q = await Model.find({}).exec();
+        return res.status(200).json(q);
+    } catch (err) {
+        return res.status(500).json({ error: err.message });
     }
 };
 
-const tripsFindByCode = async(req, res) => {
+const tripsFindByCode = async (req, res) => {
+    try {
+        const q = await Model.find({ 'code': req.params.tripCode }).exec();
+        if (!q || q.length === 0) {
+            return res.status(404).json({ error: 'Trip not found' });
+        }
+        return res.status(200).json(q);
+    } catch (err) {
+        return res.status(500).json({ error: err.message });
+    }
+};
+
+const tripsAddTrip = async (req, res) => {
+    try {
+        const newTrip = new Model({
+            code: req.body.code,
+            name: req.body.name,
+            length: req.body.length,
+            start: req.body.start,
+            resort: req.body.resort,
+            perPerson: req.body.perPerson,
+            image: req.body.image,
+            description: req.body.description
+        });
+
+        const q = await newTrip.save();
+        return res.status(201).json(q);
+    } catch (err) {
+        return res.status(400).json({ error: err.message });
+    }
+};
+
+// PUT: /trips/:tripCode - Adds a new Trip
+// Regardless of outcome, response must include HTML status code
+// and JSON message to the requesting client
+const tripsUpdateTrip = async (req, res) => {
+    // Uncomment for debugging
+    console.log(req.params);
+    console.log(req.body);
     const q = await Model
-        .find({'code' : req.params.tripCode})
+        .findOneAndUpdate(
+            { 'code': req.params.tripCode },
+            {
+                code: req.body.code,
+                name: req.body.name,
+                length: req.body.length,
+                start: req.body.start,
+                resort: req.body.resort,
+                perPerson: req.body.perPerson,
+                image: req.body.image,
+                description: req.body.description
+            }
+        )
         .exec();
 
-
-    if(!q)
-    {
+    if (!q) {
+        // Database returned no data
         return res
-                .status(404)
-                .json(err);
-    }else{
-        return res 
-            .status(200)
-            .json(q);
+            .status(400)
+            .json({ message: "Error: Trip not found or could not be updated" });
+    } else {
+        // Return resulting updated trip
+        return res
+            .status(201)
+            .json({ message: "Trip updated successfully", trip: q });
     }
+
+    // Uncomment the following line to show results of operation
+    // on the console
+    // console.log(q);
 };
 
 
 module.exports = {
     tripsList,
-    tripsFindByCode
-    
+    tripsFindByCode,
+    tripsAddTrip,
+    tripsUpdateTrip
 };
--- app_api/routes/index.js
+++ app_api/routes/index.js
@@ -5,12 +5,14 @@ const tripsController = require('../controllers/trips');
 
 router 
     .route('/trips')
-    .get(tripsController.tripsList);
+    .get(tripsController.tripsList)
+    .post(tripsController.tripsAddTrip);
 
 
 router
     .route('/trips/:tripCode')
-    .get(tripsController.tripsFindByCode);
+    .get(tripsController.tripsFindByCode)
+    .put(tripsController.tripsUpdateTrip);
 
 
 module.exports = router;
